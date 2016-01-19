f95d6510959aa606071cd3fe1a90faf16d7e50ce
piperauritum <iqed@de-dicto.net>
2016-01-20 00:49:48 +0900

    no message

--- tuple.rb
+++ tuple.rb
@@ -23,41 +23,36 @@ d = dur.map{|e| qt * e * 1/tpl_ratio * tpl_nval}
 # p d				# each note values
 # p d.inject(:+)	# total note values
 
-def new_note_value(tpl_rto, tpl_nval)
+def note_value(rto_num, rto_den, tpl_nval)
 
-	vd = [*0..6].map{|e|	# duple notes
+	duple_note = [*0..6].map{|e|
 		x = 2**e
 		[x, "#{PPQN*4/x}"]
 	}
 
-	vt = [*1..5].map{|e|	# dotted notes
+	dotted_note = [*1..5].map{|e|
 		x = PPQN*4/(2**e)
 		x = x.to_s + "."
 		[2**e*3/2, "#{x}"]
 	}
 
-	val = (vd+vt).sort{|x,y| x[0]<=>y[0]}	# note values
+	n_values = (duple_note + dotted_note).sort{|x,y| x[0]<=>y[0]}
 
-	tpl_num = tpl_rto.numerator
-	if tpl_num==0
+	if rto_num==0
 		nil
 	else
-		min_val = Math.log2(tpl_num).to_i
-		min_val = PPQN/2**min_val	
-
-		va = val.select{|v|
-			v[0]>=min_val && (v[0]<=min_val*tpl_num || Math.log2(tpl_num)%1==0)
+		min_val = PPQN * tpl_nval
+		va = n_values.select{|v|
+			v[0]>=min_val && (v[0]<=min_val*rto_num || Math.log2(rto_num)%1==0)
 		}
 		va = va.map{|v|
 			mul_val = Rational(v[0], min_val)
-			unit_val = Rational(PPQN * tpl_nval * tpl_rto.denominator, tpl_num)
-			p [mul_val , unit_val]
+			unit_val = Rational(PPQN * tpl_nval * rto_den, rto_num)
 			[mul_val * unit_val, v[1]]
 		}
-		va += val.select{|v| v[0]==PPQN}.map{|v| [Rational(v[0]), v[1]]}
+		va += n_values.select{|v| v[0]==PPQN}.map{|v| [Rational(v[0]), v[1]]}
 		Hash[*va.uniq.flatten]
 	end
 end
 
-
-p new_note_value(3/4r, 1/4r)
+p note_value(6, 4, 1/2r)
