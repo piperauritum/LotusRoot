c81981c69d5f3906dcdb1096cfb28dbf2308d532
piperauritum <iqed@de-dicto.net>
2016-03-01 01:15:42 +0900

    no message

--- tuple.rb
+++ tuple.rb
@@ -1,17 +1,17 @@
 require_relative 'bin/LotusRoot'
 
-dur = [*0..10].map{(rand(6)+1)*3}
+dur = [*0..10].map{rand(12)+1}
 clipbd(dur)
 
 elm = dur.map{|e| "@^\\markup{#{e}}"}
 # elm = dur.map{|e| "@"}
-tpl = [[12,12,1/8r]]
+tpl = [[4,3,1/4r]]
 pch = [12]
 
 sco = Score.new(dur, elm, tpl, pch)
 sco.autoAcc = 0
 sco.measure = [[[6],2]]
-# sco.measure = [2]
+sco.measure = [3]
 sco.rtoTuplet = 0
 
 # sco.finalBar = 4
