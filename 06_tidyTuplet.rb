e925eec9dce49f3a6a29833f2af5e58ed6150dc1
piperauritum <iqed@de-dicto.net>
2016-05-07 11:19:20 +0900

    Add 06_tidyTuplet.rb

---
+++ 06_tidyTuplet.rb
@@ -0,0 +1,14 @@
+﻿require_relative 'bin/LotusRoot'
+
+dur = [*0..99].map{rand(4)+1}
+elm = dur.map{"@"}
+tpl = [8]
+pch = [0]
+
+sco = Score.new(dur, elm, tpl, pch)
+sco.pitchShift = 12
+sco.metre = [[[4], 1/2r]]
+# sco.tidyTuplet = 0	# make tuplets readable
+sco.gen
+sco.print
+sco.export("sco.txt")
