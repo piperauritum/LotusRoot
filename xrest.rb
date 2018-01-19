6134b43dcbd54aa672ce79d3179d6fb50b0fde26
piperauritum <iqed@de-dicto.net>
2018-01-19 23:06:46 +0900

    no message

--- xrest.rb
+++ xrest.rb
@@ -1,19 +1,19 @@
-require_relative 'bin/LotusRoot'
+﻿require_relative 'bin/LotusRoot'
 require_relative 'bin/_win32'
 
 dur = [*0..499].map{(rand(16)+1)}
 
-elm = dur.map{%w(@ r! r! r!)[rand(4)]}
+elm = dur.map{%w(@ r! r! r!)[rand(2)]}
 # elm = dur.map{[["@", 1], "r!"][rand(2)]}
 
 clipbd([dur, elm])
-# dur, elm = [[1,6,1],%w(@ r! @)]
 tpl = [8]
-
+dur = [10,18,15,13]
+elm = %w(@ r! @ r!)
 pch = [24]
 sco = Score.new(dur, elm, tpl, pch)
-# sco.metre = [6]
-sco.omitRest = [3/2r]
+sco.metre = [[[7], 1/2r]]
+# sco.omitRest = [3/2r]
 # sco.tidyTuplet = 0
 # sco.beamOverRest = 0
 sco.gen
